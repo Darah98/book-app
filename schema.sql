a4987ef02c9091b6902b17323fdba17b6af268a9
darah98 <darashalabi@gmail.com>
2020-04-14 06:43:56 +0300

    lab12

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books(
+    id SERIAL PRIMARY KEY,
+    img VARCHAR(255),
+    title VARCHAR(255),
+    author VARCHAR(255),
+    description TEXT,
+    isbn VARCHAR(255),
+    bookshelf VARCHAR(255)
+);
+
+INSERT INTO books (img, title, author, description, isbn, bookshelf) VALUES ('../bookcover.jpg', 'some book', 'some author', 'some description', '516512', 'some category');
